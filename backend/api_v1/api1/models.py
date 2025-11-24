064fd8b0011fbf6c29fb97f6de3e6763d690575a
DanTar <tatasovdanil2207@gmail.com>
2025-11-24 16:53:47 +0400

    init django app

---
+++ backend/api_v1/api1/models.py
@@ -0,0 +1,93 @@
+from turtle import title
+from django.contrib.auth.hashers import check_password, make_password
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.db import models
+from django.utils import timezone
+
+# -------------- soft delete --------------
+
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+
+class AllObjectsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+
+
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = SoftDeleteManager()
+    all_objects = AllObjectsManager()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        if not self.is_deleted:
+            self.is_deleted = True
+            self.deleted_at = timezone.now()
+            self.save(update_fields=["is_deleted", "deleted_at"])
+
+
+class SoftDeleteUserModel(AbstractBaseUser):
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = SoftDeleteManager()
+    all_objects = AllObjectsManager()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        if not self.is_deleted:
+            self.is_deleted = True
+            self.deleted_at = timezone.now()
+            self.save(update_fields=["is_deleted", "deleted_at"])
+
+
+# -------------- model Users --------------
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        user = self.model(
+            username=self.model.normalize_username(username=username), **extra_fields
+        )  # Create user instance
+        user.set_password(password)  # Hash the password
+        user.save(using=self._db)  # Save to database
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(username, password, **extra_fields)
+
+
+class User(SoftDeleteUserModel):
+    username = models.CharField(unique=True, max_length=16)
+    password = models.CharField(max_length=128)
+    email = models.EmailField()
+    registration_day = models.DateField(default=timezone.now)
+
+    USERNAME_FIELD = "username"
+    objects = CustomUserManager()
+
+class Post(SoftDeleteModel):
+    title=models.CharField(max_length=32)
+
+
+class GlobalAdmin(SoftDeleteModel):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+
